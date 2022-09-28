8bbb80516b75c1a0fe12a7a0ce3f68321ec53582
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-09-28 14:55:28 -0400

    added API and tested images

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("working");
+const url = "https://thesimpsonsquoteapi.glitch.me/quotes";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((res) => console.log("success!", res))
+  .catch((err) => console.log("something went wrong...", err));
+
+let button = document.querySelector("#button");
+let image = document.querySelector(".simpsonsPic");
+let quote = document.querySelector(".quote");
+
+function getImage() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => (image.src = res[0].image));
+}
+
+button.addEventListener("click", getImage);
