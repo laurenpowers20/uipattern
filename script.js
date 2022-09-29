22511b7e3d52ce5281664a76f37099623702ec4e
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-09-29 09:55:38 -0400

    another modal pattern

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ fetch(url)
   .then((res) => console.log("success!", res))
   .catch((err) => console.log("something went wrong...", error));
 
-let button = document.querySelector(".button");
-let image = document.querySelector(".simpsons-pic");
 let name = document.querySelector(".name");
 let quote = document.querySelector(".quote");
-let simpsonsPic = document.querySelectorAll(".simpson-pic");
+let image = document.querySelector(".image");
+const modal = document.querySelector("#modal");
+const modalOverlay = document.querySelector("#modal-overlay");
+const closeButton = document.querySelector("#close-button");
+const openButton = document.querySelector("#open-button");
 
 function getCharacter() {
   fetch(url)
@@ -23,30 +25,14 @@ function getCharacter() {
     );
 }
 
-button.addEventListener("click", getCharacter);
 
-// Get the modal
-let modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+closeButton.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-button.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+openButton.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
